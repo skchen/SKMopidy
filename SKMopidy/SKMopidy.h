2a380fd15e8a7dd9c4de5c1b045ad7e58758c88a
Shin-Kai Chen <skchen@sixnology.com>
2016-04-23 13:29:51 +0800

    Add project

---
+++ SKMopidy/SKMopidy.h
@@ -0,0 +1,19 @@
+//
+//  SKMopidy.h
+//  SKMopidy
+//
+//  Created by Shin-Kai Chen on 2016/4/23.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKMopidy.
+FOUNDATION_EXPORT double SKMopidyVersionNumber;
+
+//! Project version string for SKMopidy.
+FOUNDATION_EXPORT const unsigned char SKMopidyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKMopidy/PublicHeader.h>
+
+
